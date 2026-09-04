f558073ee63020ab3f0026fdd25b43e87863e807
AbolfazlAdhami <aadhami880@gmail.com>
2026-09-05 00:18:17 +0330

    inti: initilize base api and server

---
+++ apps/api/src/app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/v1/health", (_req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
+
+export default app;
