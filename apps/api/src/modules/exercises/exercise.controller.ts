a45ec4e0d33e9ca79d62a175caad51f1bd4d1e09
AbolfazlAdhami <aadhami880@gmail.com>
2026-09-06 15:36:41 +0330

    new: error config and error midlware

--- apps/api/src/modules/exercises/exercise.controller.ts
+++ apps/api/src/modules/exercises/exercise.controller.ts
@@ -3,45 +3,22 @@ import type { Request, Response } from "express";
 import { parseExerciseQuery } from "./exercise.query";
 import { getExerciseById, getExercises } from "./exercise.service";
 
-import { NotFoundError } from "../../errors/app-error";
-
 export async function getExercisesController(req: Request, res: Response) {
-  try {
-    const query = parseExerciseQuery(req.query);
+  const query = parseExerciseQuery(req.query);
 
-    const result = await getExercises(query);
+  const result = await getExercises(query);
 
-    res.json({
-      data: result.items,
-      meta: result.meta,
-    });
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).json({
-      error: "Internal server error",
-    });
-  }
+  res.json({
+    data: result.items,
+    meta: result.meta,
+  });
 }
 
 export async function getExerciseByIdController(req: Request, res: Response) {
-  try {
-    const exerciseId = req.params.id as string;
-    const exercise = await getExerciseById(exerciseId);
+  const exerciseId = req.params.id as string;
+  const exercise = await getExerciseById(exerciseId);
 
-    res.json({
-      data: exercise,
-    });
-  } catch (error) {
-    if (error instanceof NotFoundError) {
-      res.status(error.statusCode).json({
-        error: error.message,
-      });
-
-      return;
-    }
-    res.status(500).json({
-      error: "Internal server error",
-    });
-  }
+  res.json({
+    data: exercise,
+  });
 }
